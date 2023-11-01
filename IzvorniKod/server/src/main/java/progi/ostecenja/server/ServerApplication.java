9acfe37e795f4ac8563793cd30f205e3635dbc09
Filip <fv54209@fer.hr>
2023-11-01 13:35:21 +0100

    spring server initialized

---
+++ IzvorniKod/server/src/main/java/progi/ostecenja/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package progi.ostecenja.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServerApplication.class, args);
+
+
+	}
+}
+
