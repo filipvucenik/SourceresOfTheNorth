b826c45f230ac7993a4b5d4c8c08d4049acd3d76
Filip <fv54209@fer.hr>
2023-11-01 20:48:31 +0100

    feedback and image

--- IzvorniKod/server/src/main/java/com/progi/ostecenja/server/repo/Feedback.java
+++ IzvorniKod/server/src/main/java/com/progi/ostecenja/server/repo/Feedback.java
@@ -1,4 +1,61 @@
 package com.progi.ostecenja.server.repo;
 
+import jakarta.persistence.JoinColumn;
+
+import java.sql.Timestamp;
+
 public class Feedback {
+
+    @JoinColumn(name ="groupID")
+    private Long groupID;
+
+    @JoinColumn(name = "cityOfficeID")
+    private Long cityOfficeID;
+
+    private String status;
+
+    private Timestamp changeTS;
+
+    public Feedback(Long groupID, Long cityOfficeID, String status, Timestamp changeTS) {
+        this.groupID = groupID;
+        this.cityOfficeID = cityOfficeID;
+        this.status = status;
+        this.changeTS = changeTS;
+    }
+
+    public Feedback(){
+
+    }
+
+    public Long getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(Long groupID) {
+        this.groupID = groupID;
+    }
+
+    public Long getCityOfficeID() {
+        return cityOfficeID;
+    }
+
+    public void setCityOfficeID(Long cityOfficeID) {
+        this.cityOfficeID = cityOfficeID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getChangeTS() {
+        return changeTS;
+    }
+
+    public void setChangeTS(Timestamp changeTS) {
+        this.changeTS = changeTS;
+    }
 }
--- IzvorniKod/server/src/main/java/com/progi/ostecenja/server/repo/Image.java
+++ IzvorniKod/server/src/main/java/com/progi/ostecenja/server/repo/Image.java
@@ -1,4 +1,48 @@
 package com.progi.ostecenja.server.repo;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+
 public class Image {
+
+    @Id
+    private Long imageID;
+
+    @JoinColumn(name ="reportID")
+    private Long reportID;
+
+    private String URL;
+
+    public Image (){
+
+    }
+    public Image(Long imageID, Long reportID, String URL) {
+        this.imageID = imageID;
+        this.reportID = reportID;
+        this.URL = URL;
+    }
+
+    public Long getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(Long imageID) {
+        this.imageID = imageID;
+    }
+
+    public Long getReportID() {
+        return reportID;
+    }
+
+    public void setReportID(Long reportID) {
+        this.reportID = reportID;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
 }
