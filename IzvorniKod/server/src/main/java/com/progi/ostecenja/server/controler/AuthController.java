9fa8b8a71ee77329f577cd9eeb9852279ee52e74
meglicfran <meglic.fran@gmail.com>
2023-11-13 20:08:58 +0100

    endpoint za login i registraciju

--- IzvorniKod/server/src/main/java/com/progi/ostecenja/server/controler/AuthController.java
+++ IzvorniKod/server/src/main/java/com/progi/ostecenja/server/controler/AuthController.java
@@ -5,6 +5,7 @@ import com.progi.ostecenja.server.repo.Users;
 import com.progi.ostecenja.server.service.CityOfficeService;
 import com.progi.ostecenja.server.service.UsersService;
 import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +19,11 @@ public class AuthController {
     @Autowired
     private UsersService usersService;
     private CityOfficeService officeService;
+
     @PostMapping("/userLogin")
     @ResponseBody
     public ResponseEntity<String> loginAuth(HttpSession session, @RequestBody UserCredentials userCredentials){
+
         String username= userCredentials.username;
         if(session.getAttribute("USER")!=null)
             return new ResponseEntity<>(session.getAttribute("USER").toString(), HttpStatus.OK);
@@ -48,7 +51,28 @@ public class AuthController {
         session.setAttribute("OFFICE",office.getCityOfficeId());
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
+    
+    @PostMapping("/userRegister")
+    public ResponseEntity<String> userRegister(HttpSession session, @RequestBody Users user) {
+        if (session.getAttribute("USER") != null)
+            return new ResponseEntity<>(session.getAttribute("USER").toString(), HttpStatus.OK);
+
+        Users createdUser = null;
+        try {
+            createdUser = usersService.createUser(user);
+        } catch (Exception e) {
+
+        }
+
+        if (createdUser!=null){
+            return new ResponseEntity<>("Success", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Bad request", HttpStatus.BAD_REQUEST);
+    }
+
 }
+
+
 class UserCredentials {
     public String username;
     public String password;
