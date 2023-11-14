40b37f2f63d5bd9bf86857dcddff829e119c9ad2
b-mikulan <bruno.mikulan@fer.hr>
2023-11-14 14:06:26 +0100

    React router

---
+++ IzvorniKod/frontend/src/Admin.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "./admin_view.css";
+
+const ReportListElement = ({ repo }) => {
+  return (
+    <div className="report">
+      <div className="report-info">{repo.name}</div>
+      <div className="report-info">{repo.kategorija}</div>
+      <div className="report-info">{repo.lokacija}</div>
+      <div className="report-info">{repo.status}</div>
+    </div>
+  );
+};
+
+function Admin() {
+  const data = [
+    {
+      id: 1,
+      name: "problem s vodom",
+      status: "aktivna",
+      lokacija: "vrbik",
+      kategorija: "voda",
+    },
+    {
+      id: 2,
+      name: "problem s vodom",
+      status: "aktivna",
+      lokacija: "FER",
+      kategorija: "voda",
+    },
+    {
+      id: 3,
+      name: "problem s vodom",
+      status: "done",
+      lokacija: "knezija",
+      kategorija: "voda",
+    },
+  ];
+  const [displaied_data, set_display] = useState([]);
+  const updateDisplay = (status) => {
+    set_display(
+      data.filter((repo) => {
+        if (repo.status === status) {
+          return true;
+        } else return false;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <h1>Admin view all reports</h1>
+      <div className="Sorting_buttons">
+        <button onClick={() => updateDisplay("aktivna")}>Aktivne</button>
+        <button onClick={() => updateDisplay("wait")}>Na čekanju</button>
+        <button onClick={() => updateDisplay("done")}>Riješene</button>
+      </div>
+      {displaied_data.length > 0 && (
+        <div className="report-columns">
+          <div className="report-info-column">Naziv</div>
+          <div className="report-info-column">Kategorija</div>
+          <div className="report-info-column">Lokacija</div>
+          <div className="report-info-column">Status</div>
+        </div>
+      )}
+      <div className="report-list">
+        <ul>
+          {displaied_data.map((repo) => (
+            <li key={repo.id}>
+              <ReportListElement repo={repo}></ReportListElement>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Admin;
--- IzvorniKod/frontend/src/App.jsx
+++ IzvorniKod/frontend/src/App.jsx
@@ -1,79 +1,23 @@
-import { useState } from "react";
-import "./admin_view.css";
+import React from "react";
+import MapComponent from "./MapComponent.jsx";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+// import PrijavaSteteComponent from "./PrijavaSteteComponent.jsx";
+// import LoginComponent from "./LoginComponent.jsx";
 
-const ReportListElement = ({ repo }) => {
+import Admin from "./Admin.jsx";
+import Main from "./Main.jsx";
+import Reports from "./Report.jsx";
+
+const App = () => {
   return (
-    <div className="report">
-      <div className="report-info">{repo.name}</div>
-      <div className="report-info">{repo.kategorija}</div>
-      <div className="report-info">{repo.lokacija}</div>
-      <div className="report-info">{repo.status}</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/report" element={<Reports />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
-
-function App() {
-  const data = [
-    {
-      id: 1,
-      name: "problem s vodom",
-      status: "aktivna",
-      lokacija: "vrbik",
-      kategorija: "voda",
-    },
-    {
-      id: 2,
-      name: "problem s vodom",
-      status: "aktivna",
-      lokacija: "FER",
-      kategorija: "voda",
-    },
-    {
-      id: 3,
-      name: "problem s vodom",
-      status: "done",
-      lokacija: "knezija",
-      kategorija: "voda",
-    },
-  ];
-  const [displaied_data, set_display] = useState([]);
-  const updateDisplay = (status) => {
-    set_display(
-      data.filter((repo) => {
-        if (repo.status === status) {
-          return true;
-        } else return false;
-      })
-    );
-  };
-
-  return (
-    <div>
-      <h1>Admin view all reports</h1>
-      <div className="Sorting_buttons">
-        <button onClick={() => updateDisplay("aktivna")}>Aktivne</button>
-        <button onClick={() => updateDisplay("wait")}>Na čekanju</button>
-        <button onClick={() => updateDisplay("done")}>Riješene</button>
-      </div>
-      {displaied_data.length > 0 && (
-        <div className="report-columns">
-          <div className="report-info-column">Naziv</div>
-          <div className="report-info-column">Kategorija</div>
-          <div className="report-info-column">Lokacija</div>
-          <div className="report-info-column">Status</div>
-        </div>
-      )}
-      <div className="report-list">
-        <ul>
-          {displaied_data.map((repo) => (
-            <li key={repo.id}>
-              <ReportListElement repo={repo}></ReportListElement>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
 
 export default App;
