c5cedc7b62cfb899d4a4118b2ba730bc7433da11
v-brkic <vinko.brkic337@gmail.com>
2023-11-15 20:02:25 +0100

    popravljene greškice i dodan StatisticComponent

--- IzvorniKod/frontend/src/App.jsx
+++ IzvorniKod/frontend/src/App.jsx
@@ -1,27 +1,47 @@
-import React from "react";
-import MapComponent from "./MapComponent.jsx";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-// import PrijavaSteteComponent from "./PrijavaSteteComponent.jsx";
-// import LoginComponent from "./LoginComponent.jsx";
+import React from 'react';
+import MapComponent from './MapComponent.jsx';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import PrijavaSteteComponent from './Rforma.jsx';
+import LoginComponent from './Lforma.jsx';
+import StatisticComponent from './StatisticComponent.jsx';
+import './App.css';
 
-import Admin from "./Admin.jsx";
-import Main from "./Main.jsx";
-import Reports from "./Report.jsx";
-import Lforma from "./Lforma.jsx";
-import Rforma from "./Rforma.jsx";
 
-const App = () => {
+function App() {
+  const signedIn = false;
   return (
-    <BrowserRouter>
+    <Router>
+    <div className="App">
+      <div className='title'><h1> Mapa prijavljenih šteta</h1></div>
+      <div className='header'>
+        <div className='left'>
+          <Link to="/prijava" className="profile-button"><button className='prijavaStete'>Prijava štete</button></Link>
+          <Link to="/statistika" className="profile-button"><button className='prijavaStete'>Statistika</button></Link>
+        </div>
+          {signedIn ? (
+            <Link to="/profile" className="profile-button"><button className='username'>Prijavljeni korisnik</button></Link>
+          ) : (
+            <>
+            <div className='right'>
+              <Link to="/login" className="profile-button"><button className='login'>Log in</button></Link>
+              <Link to="/registracija" className="profile-button"><button className='login'>Register</button></Link>
+            </div>
+            </>
+          )}
+        </div>
+      <div className='footer'></div>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/report" element={<Reports />} />
-        <Route path="/login" element={<Lforma />} />
-        <Route path="/prijava" element={<Rforma></Rforma>} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/" element={<MapComponent />} />
+          <Route path="/prijava" element={<PrijavaSteteComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/statistika" element={<StatisticComponent />} />
+          {/*}<Route path="/registracija" element={<RegisterComponent />} />
+          <Route path="/profile" element={<ProfilePage />} />{*/}
+    </Routes>
+    </div>
+    </Router>
   );
-};
+}
 
 export default App;
+
--- IzvorniKod/frontend/src/MapComponent.jsx
+++ IzvorniKod/frontend/src/MapComponent.jsx
@@ -1,39 +1,36 @@
-import React, { useEffect, useRef } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-//import markerIcon from './marker.svg';
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon from './marker.svg';
 
 function MapComponent() {
   const mapRef = useRef(null); // referenca za spremanje instance karte
   const markers = []; // Niz za pohranu svih markera
 
-  useEffect(() => {
-    if (!mapRef.current) {
-      // stvaranje karte samo ako ref nije postavljen (MapComponent already initialized error)
-      const map = L.map("map").setView([45.8, 15.967], 13); //stvaranje mape s centrom na Zagrebu
+  const uniqueMapId = `map-${Math.floor(Math.random() * 10000)}`;
 
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        //dodavanje plocice karti
+  useEffect(() => {
+    if (!mapRef.current) { // stvaranje karte samo ako ref nije postavljen (MapComponent already initialized error)
+      const map = L.map(uniqueMapId).setView([45.800, 15.967], 13);//stvaranje mape s centrom na Zagrebu
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //dodavanje plocice karti
         maxZoom: 19,
       }).addTo(map);
 
-      var customIcon = L.divIcon({
-        //ikona za prikaz markera
+      var customIcon = L.divIcon({ //ikona za prikaz markera
         html: `<div class="marker-container">
-                 <img class="marker-icon" src="https://picsum.photos/20/30
-                 " />
+                 <img class="marker-icon" src="${markerIcon}" />
                </div>`,
-        //<span class="delete-marker" style="position: absolute; top: -13px; right: -6px; cursor: pointer; color: red; font-size: 20px;">x</span>
+               //<span class="delete-marker" style="position: absolute; top: -13px; right: -6px; cursor: pointer; color: red; font-size: 20px;">x</span>
         iconSize: [26, 26],
         iconAnchor: [13, 26], // promijenjen "anchor point" kako bi se ikona centrirala na točku
       });
 
-      const createMarker = (lat, lng) => {
-        //funkcija prikazivanja markera na temelju koordinata
+      const createMarker = (lat, lng) => { //funkcija prikazivanja markera na temelju koordinata
         const marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
         markers.push(marker);
 
-        /* marker.on('mouseover', function () { //prikaz gumba za brisanje markera kad je marker hoveran
+       /* marker.on('mouseover', function () { //prikaz gumba za brisanje markera kad je marker hoveran
           const deleteButton = marker.getElement().querySelector('.delete-marker');
           if (deleteButton) {
             deleteButton.style.display = 'block';
@@ -53,30 +50,25 @@ function MapComponent() {
 
         let popupIsOpen = false;
 
-        marker.on("click", () => {
-          //otvaranje popUp-a svaki put kad se klikne na marker
+        marker.on('click', () => { //otvaranje popUp-a svaki put kad se klikne na marker
           if (popupIsOpen) {
             marker.closePopup();
             popupIsOpen = false;
           } else {
-            marker
-              .bindPopup(
-                `Naslov prijave, kratki opis bude tu + <br/><a href=''>Otvori stranicu prijave</a>`
-              )
-              .openPopup();
+            marker.bindPopup(`Naslov prijave, kratki opis bude tu + <br/><a href=''>Otvori stranicu prijave</a>`).openPopup();
             popupIsOpen = true;
           }
         });
       };
       //ovdje ce se dodavati markeri na koordinate prijavljenih steta
-      var jason =
-        '[{"lat":45.800, "lng" :15.967},{"lat":45.800, "lng":15.969}, {"lat":45.801, "lng":15.971}]';
-
+      var jason = '[{"lat":45.800, "lng" :15.967},{"lat":45.800, "lng":15.969}, {"lat":45.801, "lng":15.971}]';
+      
       var lokacije = JSON.parse(jason);
       console.log(lokacije);
-      for (var lokacija of lokacije) {
-        createMarker(lokacija.lat, lokacija.lng);
+      for(var lokacija of lokacije){
+        createMarker(lokacija.lat,lokacija.lng);
       }
+      
 
       /*function deleteMarker(markerToDelete) { //brisanje markera i izbacivanje iz liste
         mapRef.current.removeLayer(markerToDelete);
@@ -92,7 +84,7 @@ function MapComponent() {
     }
   }, []);
 
-  return <div id="map" style={{ width: "90%", height: "70vh" }}></div>;
+  return  <div id={uniqueMapId} style={{ width: '90%', height: '70vh' }}></div>;
 }
 
 export default MapComponent;
