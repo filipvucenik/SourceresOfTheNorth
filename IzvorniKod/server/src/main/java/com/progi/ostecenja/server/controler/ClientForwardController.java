71e96bab96dae9f065bc534996b441f848b85d1f
Filip <filip.vucenik@gmail.com>
2023-11-16 22:42:24 +0100

    Frontend i backend merge

---
+++ IzvorniKod/server/src/main/java/com/progi/ostecenja/server/controler/ClientForwardController.java
@@ -0,0 +1,12 @@
+package com.progi.ostecenja.server.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientForwardController {
+    @GetMapping(value = "/**/{path:[^\\.]*")
+    public String forward(){
+        return "forward:/";
+    }
+}
