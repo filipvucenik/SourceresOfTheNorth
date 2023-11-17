d56f1b6caea6c0ac6103c21925b459792cc05afc
wowow-02 <148803287+wowow-02@users.noreply.github.com>
2023-11-17 19:39:57 +0100

    Add files via upload
    dodan /reports path

--- IzvorniKod/server/src/main/frontend/src/apiConfig.js
+++ IzvorniKod/server/src/main/frontend/src/apiConfig.js
@@ -4,4 +4,5 @@ export default {
   getUrl: `${API_BASE_URL}`,
   getLoginUrl: `${API_BASE_URL}/auth/userLogin`,
   getRegisterUrl: `${API_BASE_URL}/auth/userRegister`,
+  getReportUrl: `${API_BASE_URL}/report`
 };
