003c8b2d1d481df66c1e23fe63f2435858170d47
wowow-02 <148803287+wowow-02@users.noreply.github.com>
2023-11-17 14:17:29 +0100

    Add files via upload
    Nova forma za prijavu štete

--- IzvorniKod/server/src/main/frontend/src/Rforma.jsx
+++ IzvorniKod/server/src/main/frontend/src/Rforma.jsx
@@ -1,57 +1,109 @@
-import "./Rforma.css";
-const Rforma = () => {
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Rforma.css'
+const ReportCard = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [location, setLocation] = useState({ lat: 45.804270084085914, lng: 15.978798866271974 }); // Default na Zg
+  const [picture, setPicture] = useState(null);
+  const [manualAddress, setManualAddress] = useState('');
+
+  const handleMapClick = async (e) => {
+    const clickedLatLng = e.latlng;
+    setLocation({
+      lat: clickedLatLng.lat,
+      lng: clickedLatLng.lng,
+    });
+
+    // GEOCODING API
+    const apiKey = '7fbe9533c0c9424aa41c500419e5ef83';
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${clickedLatLng.lat}+${clickedLatLng.lng}&key=${apiKey}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.results.length > 0) {
+        const formattedAddress = data.results[0].formatted;
+        setManualAddress(formattedAddress);
+      }
+    } catch (error) {
+      console.error('Error fetching address:', error);
+    }
+  };
+
+  const handlePictureChange = (file) => {
+    setPicture(file);
+  };
+
+  const handleSubmit = () => {
+    console.log('Report submitted:', {
+      title,
+      description,
+      category,
+      location,
+      picture,
+      manualAddress,
+    });
+    // Trebam Filipa...
+  };
+
+  
+  function MapClickHandler() {
+    useMapEvents({
+      click: handleMapClick,
+    });
+
+    return null;
+  }
+
+  useEffect(() => {
+    setManualAddress('');
+  }, [location]);
+
   return (
-    <div className="report-form">
-      <h1>Prijava štete na cestama i javnim površinama</h1>
-      <form action="#" method="post">
-        <div className="form-group">
-          <label htmlFor="office">Nadležan ured:</label>
-          <select id="office" name="office" required="">
-            <option value="" disabled="" selected="">
-              Ručni odabir ureda
-            </option>
-            <option value="office1">Ured 1</option>
-            <option value="office2">Ured 2</option>
-            <option value="office3">Ured 3</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="problem">Problem:</label>
-          <input type="text" id="problem" name="problem" required="" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="description">Opis:</label>
-          <textarea
-            id="description"
-            name="description"
-            rows={4}
-            required=""
-            defaultValue={""}
-          />
-        </div>
-        <div className="form-group">
-          <label>Lokacija:</label>
-          <div className="map-container" id="map" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="picture">Priloži sliku:</label>
-          <input type="file" id="picture" name="picture" />
-        </div>
-        <button type="submit" className="submit-btn">
-          Predaj
-        </button>
-        <div className="form-group">
-          <label htmlFor="additional-info">Dodatne informacije(Izborno):</label>
-          <textarea
-            id="additional-info"
-            name="additional-info"
-            rows={4}
-            defaultValue={""}
-          />
-        </div>
-      </form>
+    <div className="report-card">
+      <h2>Prijava oštečenja</h2>
+
+      <label htmlFor="title">Naslov:</label>
+      <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+      <label htmlFor="description">Opis:</label>
+      <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+
+      <label htmlFor="category">Kategorija:</label>
+      <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="category1">Kategorija 1</option>
+        <option value="category2">Kategorija 1</option>
+        <option value="category3">Kategorija 1</option>
+      </select>
+      <label htmlFor="picture">Prikvači sliku:</label>
+      <input type="file" id="picture" accept="image/*" onChange={handlePictureChange} />
+      <MapContainer
+        center={[location.lat, location.lng]}
+        zoom={13}
+        style={{ height: '300px', width: '100%', margin: '10px 0' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <MapClickHandler />
+        <Marker position={location}>
+          <Popup>Odaberi lokaciju</Popup>
+        </Marker>
+      </MapContainer>
+
+      <div>
+        <label htmlFor="address">Upiši adresu:</label>
+        <input type="text" id="address" value={manualAddress} readOnly />
+      </div>
+      
+      <button onClick={handleSubmit}>Predaj prijavu</button>
     </div>
   );
 };
 
-export default Rforma;
+export default ReportCard;
