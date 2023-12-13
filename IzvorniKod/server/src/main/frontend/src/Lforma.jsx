899142a7a70994e09af50da9af790ee169ec1c35
v-brkic <vinko.brkic337@gmail.com>
2023-12-13 17:55:13 +0100

    slozen header

--- IzvorniKod/server/src/main/frontend/src/Lforma.jsx
+++ IzvorniKod/server/src/main/frontend/src/Lforma.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Link } from "react";
 import Cookies from "js-cookie";
 import "./Lforma.css";
 import { useNavigate } from "react-router-dom";
 import apiConfig from "./apiConfig";
-import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
+import HeaderComponent from "./HeaderComponent";
 
 const LoginComponent = () => {
   const [password, setPassword] = useState("");
@@ -60,6 +60,49 @@ const LoginComponent = () => {
 
   return (
     <>
+      <header className="p-3 text-bg-dark">
+        <div className="container">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a
+              href="/"
+              className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+            >
+              <svg
+                className="bi me-2"
+                width="40"
+                height="32"
+                role="img"
+                aria-label="Bootstrap"
+              >
+                <use href="#bootstrap"></use>
+              </svg>
+            </a>
+            <a href="/">
+              <img src="logo2.png" alt="logo2" classNameName="mx-4" />
+            </a>
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li>
+                <a href="#" className="nav-link px-2 text-secondary">
+                  <a href="/prijava">
+                    <button type="button" className="btn btn-outline-light me-2">
+                      Prijava štete
+                    </button>
+                  </a>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav-link px-2 text-white">
+                  <a href="/statistika">
+                    <button type="button" className="btn btn-outline-light me-2">
+                      Statistika
+                    </button>
+                  </a>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
       <div className="form-container">
         <form className="login-form">
           <h2>Prijava</h2>
@@ -82,7 +125,7 @@ const LoginComponent = () => {
           </button>
         </form>
       </div>
-      <FooterComponent/>
+      <FooterComponent />
     </>
   );
 };
