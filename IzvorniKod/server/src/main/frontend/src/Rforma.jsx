f7959c138054a2ffb4eea1a3e0181646a038b0a3
b-mikulan <bruno.mikulan@fer.hr>
2023-12-30 14:12:05 +0100

    postgres server adjustment

--- IzvorniKod/server/src/main/frontend/src/Rforma.jsx
+++ IzvorniKod/server/src/main/frontend/src/Rforma.jsx
@@ -1,38 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import './Rforma.css'
-import L from 'leaflet';
-import markerIcon from'./marker.svg';
+import React, { useState, useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./Rforma.css";
+import L from "leaflet";
+import markerIcon from "./marker.svg";
 import apiConfig from "./apiConfig";
-import FooterComponent from './FooterComponent';
-import HeaderComponent from './HeaderComponent';
+import FooterComponent from "./FooterComponent";
+import HeaderComponent from "./HeaderComponent";
 
-let categoryData="";
-try{
-  const fetchCategory=await fetch('https://ostecenja-progi-fer.onrender.com/category')
-  const fetchData= await fetchCategory.json();
-  categoryData=Object.fromEntries(
-    fetchData.map(item => [item.categoryID, item.categoryName])
+let categoryData = "";
+try {
+  const fetchCategory = await fetch(
+    "https://ostecenja-progi-fer.onrender.com/category"
   );
-}catch(error){
+  const fetchData = await fetchCategory.json();
+  categoryData = Object.fromEntries(
+    fetchData.map((item) => [item.categoryID, item.categoryName])
+  );
+} catch (error) {
   console.error(error);
-  alert("Greška prilikom dohvaćanja kategorija, molimo osvježite stranicu")
+  alert("Greška prilikom dohvaćanja kategorija, molimo osvježite stranicu");
 }
 
-let keyWordData="";
-try{
-  const fecthKeyData=await fetch('https://ostecenja-progi-fer.onrender.com/keywords')
-  const fetchData=await fecthKeyData.json();
-  keyWordData=Object.fromEntries(
-    fetchData.map(item => [item.keyword.toLowerCase(), item.categoryID])
+let keyWordData = "";
+try {
+  const fecthKeyData = await fetch(
+    "https://ostecenja-progi-fer.onrender.com/keywords"
   );
-
-}catch(error){
+  const fetchData = await fecthKeyData.json();
+  keyWordData = Object.fromEntries(
+    fetchData.map((item) => [item.keyword.toLowerCase(), item.categoryID])
+  );
+} catch (error) {
   console.log(error);
-  alert("Greška prilikom učitavanja stranice.\n Automatski odabir kategorije neće raditi")
+  alert(
+    "Greška prilikom učitavanja stranice.\n Automatski odabir kategorije neće raditi"
+  );
 }
-const pinpointIconUrl = markerIcon; 
+const pinpointIconUrl = markerIcon;
 const customIcon = new L.Icon({
   iconUrl: pinpointIconUrl,
   iconSize: [32, 32],
@@ -41,14 +52,17 @@ const customIcon = new L.Icon({
 });
 
 const ReportCard = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [category, setCategory] = useState('');
-  const [location, setLocation] = useState({ lat: 45.804270084085914, lng: 15.978798866271974 }); // Default na Zg
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [location, setLocation] = useState({
+    lat: 45.804270084085914,
+    lng: 15.978798866271974,
+  }); // Default na Zg
   const [picture, setPicture] = useState(null);
-  const [manualAddress, setManualAddress] = useState('');
-  
-  const manualAddressChange = (e) =>{
+  const [manualAddress, setManualAddress] = useState("");
+
+  const manualAddressChange = (e) => {
     setManualAddress(e.target.value);
   };
 
@@ -59,10 +73,8 @@ const ReportCard = () => {
       lng: clickedLatLng.lng,
     });
 
-    
-
     // GEOCODING API
-    const apiKey = '7fbe9533c0c9424aa41c500419e5ef83';
+    const apiKey = "7fbe9533c0c9424aa41c500419e5ef83";
     const url = `https://api.opencagedata.com/geocode/v1/json?q=${clickedLatLng.lat}+${clickedLatLng.lng}&key=${apiKey}`;
 
     try {
@@ -74,28 +86,25 @@ const ReportCard = () => {
         setManualAddress(formattedAddress);
       }
     } catch (error) {
-      console.error('Error fetching address:', error);
+      console.error("Error fetching address:", error);
     }
   };
-  
-  const checkForKeyword = (text) => {
 
+  const checkForKeyword = (text) => {
     const words = text.trim().toLowerCase().split(/\s+/);
-    
 
     const categoryCounts = {};
-  
-    words.forEach(word => {
+
+    words.forEach((word) => {
       if (keyWordData.hasOwnProperty(word)) {
         const categoryId = keyWordData[word];
         categoryCounts[categoryId] = (categoryCounts[categoryId] || 0) + 1;
       }
     });
-  
 
     let maxCategory = null;
     let maxCount = 0;
-    
+
     for (const categoryId in categoryCounts) {
       if (categoryCounts[categoryId] > maxCount) {
         maxCategory = categoryId;
@@ -107,15 +116,13 @@ const ReportCard = () => {
       setCategory(maxCategory.toString());
     }
   };
-  
 
-    
   const handlePictureChange = (file) => {
     setPicture(file);
   };
 
   const handleSubmit = () => {
-    console.log('Report submitted:', {
+    console.log("Report submitted:", {
       title,
       description,
       category,
@@ -123,15 +130,14 @@ const ReportCard = () => {
       picture,
       manualAddress,
     });
-    
-    const jsonServerSendData={
-      "reportHeadline": title,
-      "location":  location.lat+','+location.lng,
-      "description":description,
-      "categoryID" : 1
 
+    const jsonServerSendData = {
+      reportHeadline: title,
+      location: location.lat + "," + location.lng,
+      description: description,
+      categoryID: 1,
     };
-    
+
     let url = apiConfig.getReportUrl;
     fetch(url, {
       method: "POST",
@@ -140,18 +146,15 @@ const ReportCard = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(jsonServerSendData),
-    })
-      .then((response) => {
-        if(response.status===200){
-            alert("Vaša prijava je podnešena")
-        }else{
-            alert("DOŠLO JE DO GREŠKE!!!");
-        }
-      });
-
+    }).then((response) => {
+      if (response.status === 200) {
+        alert("Vaša prijava je podnešena");
+      } else {
+        alert("DOŠLO JE DO GREŠKE!!!");
+      }
+    });
   };
 
-  
   function MapClickHandler() {
     useMapEvents({
       click: handleMapClick,
@@ -161,58 +164,83 @@ const ReportCard = () => {
   }
 
   useEffect(() => {
-    setManualAddress('');
+    setManualAddress("");
   }, [location]);
 
   return (
     <>
-    <HeaderComponent/>
-    <div className="report-card">
-      <h2>Prijava oštečenja</h2>
+      <HeaderComponent />
+      <div className="report-card">
+        <h2>Prijava oštečenja</h2>
 
-      <label htmlFor="title">Naslov:</label>
-      <input type="text" id="title" value={title} onChange={(e) => {setTitle(e.target.value);checkForKeyword(e.target.value);}} />
-
-      <label htmlFor="description">Opis:</label>
-      <textarea id="description" value={description} onChange={(e) => {setDescription(e.target.value);checkForKeyword(e.target.value);}} />
-
-      <label htmlFor="category">Kategorija:</label>
-      <select
-        id="category"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        {Object.entries(categoryData).map(([categoryId, categoryName]) => (
-          <option key={categoryId} value={categoryId}>
-            {categoryName}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="picture">Prikvači sliku:</label>
-      <input type="file" id="picture" accept="image/*" onChange={handlePictureChange} />
-      <MapContainer
-        center={[location.lat, location.lng]}
-        zoom={13}
-        style={{ height: '300px', width: '100%', margin: '10px 0' }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        <label htmlFor="title">Naslov:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            checkForKeyword(e.target.value);
+          }}
         />
-        <MapClickHandler />
-        <Marker position={location} icon={customIcon}>
-          <Popup>Odaberi lokaciju</Popup>
-        </Marker>
-      </MapContainer>
 
-      <div>
-        <label htmlFor="address">Upiši adresu:</label>
-        <input type="text" id="address" value={manualAddress} onChange={manualAddressChange} />
+        <label htmlFor="description">Opis:</label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+            checkForKeyword(e.target.value);
+          }}
+        />
+
+        <label htmlFor="category">Kategorija:</label>
+        <select
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {Object.entries(categoryData).map(([categoryId, categoryName]) => (
+            <option key={categoryId} value={categoryId}>
+              {categoryName}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="picture">Prikvači sliku:</label>
+        <input
+          type="file"
+          id="picture"
+          accept="image/*"
+          onChange={handlePictureChange}
+        />
+        <MapContainer
+          center={[location.lat, location.lng]}
+          zoom={13}
+          style={{ height: "300px", width: "100%", margin: "10px 0" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <MapClickHandler />
+          <Marker position={location} icon={customIcon}>
+            <Popup>Odaberi lokaciju</Popup>
+          </Marker>
+        </MapContainer>
+
+        <div>
+          <label htmlFor="address">Upiši adresu:</label>
+          <input
+            type="text"
+            id="address"
+            value={manualAddress}
+            onChange={manualAddressChange}
+          />
+        </div>
+
+        <button onClick={handleSubmit}>Predaj prijavu</button>
       </div>
-      
-      <button onClick={handleSubmit}>Predaj prijavu</button>
-    </div>
-    <FooterComponent/>
+      <FooterComponent />
     </>
   );
 };
