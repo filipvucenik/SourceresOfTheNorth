bf05b213995b086d5f3425ab5eaac5cb6c21c633
v-brkic <vinko.brkic337@gmail.com>
2024-01-10 19:10:23 +0100

    napravljeno editanje korisnickih podataka

--- IzvorniKod/server/src/main/frontend/src/Profile.jsx
+++ IzvorniKod/server/src/main/frontend/src/Profile.jsx
@@ -9,21 +9,20 @@ const server = apiConfig.getUserInfoUrl;
 
 const Profile = () => {
   const [postojiKolacic, postaviPostojiKolacic] = useState(false);
-  
-
   const [userData, setUserData] = useState({
     firstName: "",
     lastName: "",
     password: "",
     email: "",
   });
-  var id;
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const kolacici = Cookies.get();
     console.log(kolacici);
-    id=kolacici.id;
-    if (Object.keys(kolacici).length > 0) {
+
+    if (kolacici && kolacici.id) {
+      setId(kolacici.id);
       postaviPostojiKolacic(true);
     }
   }, []);
@@ -41,90 +40,123 @@ const Profile = () => {
       }
     };
     fetchDataFromDatabase();
-  }, []);
-  
+  }, [id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
+
   const updateDataInDatabase = async (e) => {
     e.preventDefault();
-  
-    const firstName = e.target.elements.firstName.value;
-    const lastName = e.target.elements.lastName.value;
-    const password = e.target.elements.password.value;
-    const email = e.target.elements.email.value;
-  
-    const updatedData = {
-      firstName,
-      lastName,
-      email,
-    };
-  
-    if (password.trim() !== "") {
-      updatedData.password = password;
-    }
-  
+
     try {
       const response = await fetch(`${server}/${id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(updatedData),
+        body: JSON.stringify(userData),
       });
-  
+
       if (response.ok) {
         console.log("Podaci uspješno ažurirani.");
-        setUserData(updatedData);
+        setUserData(userData);
       } else {
         console.error("Greška prilikom ažuriranja podataka.");
       }
     } catch (error) {
       console.error("Greška pri ažuriranju podataka:", error);
     }
-  };  
+  };
+  
 
   return (
     <>
     <HeaderComponent/>
-      <div className='najjaciDiv'>
+    <div className='najjaciDiv'>
         <form>
           <div className="row g-3">
             <div className="col-sm-6">
-              <label for="firstName" className="form-label">Ime</label>
-              <input type="text" className="form-control" name="firstName" defaultValue={userData.firstName || ""}/>
+              <label htmlFor="firstName" className="form-label">
+                Ime
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="firstName"
+                value={userData.firstName}
+                onChange={handleChange}
+              />
               <div className="invalid-feedback">
                 Valid first name is required.
               </div>
             </div>
 
             <div className="col-sm-6">
-              <label for="lastName" className="form-label">Prezime</label>
-              <input type="text" className="form-control" name="lastName" defaultValue={userData.lastName || ""}/>
+              <label htmlFor="lastName" className="form-label">
+                Prezime
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="lastName"
+                value={userData.lastName}
+                onChange={handleChange}
+              />
               <div className="invalid-feedback">
                 Valid last name is required.
               </div>
             </div>
 
             <div className="col-12">
-              <label for="email" className="form-label">Email</label>
-              <input type="email" className="form-control" name="email" defaultValue={userData.email || ""}/>
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                value={userData.email}
+                onChange={handleChange}
+              />
               <div className="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
             </div>
 
             <div className="col-12">
-              <label for="password" className="form-label">Lozinka</label>
+              <label htmlFor="password" className="form-label">
+                Lozinka
+              </label>
               <div className="input-group has-validation">
-                <input type="text" className="form-control" id="password"/>
-              <div className="invalid-feedback">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  onChange={handleChange}
+                />
+                <div className="invalid-feedback">
                   Your password is required.
                 </div>
               </div>
             </div>
-            
           </div>
           <hr className="my-4"/>
-          <button type="button" className="btn btn-outline-dark me-2" onClick={updateDataInDatabase} >Spremi</button>
-          <button type="button" className="btn btn-outline-dark me-2">Obriši račun</button>
+          <button
+            type="button"
+            className="btn btn-outline-dark me-2"
+            onClick={updateDataInDatabase}
+          >
+            Spremi
+          </button>
+          <button type="button" className="btn btn-outline-dark me-2">
+            Obriši račun
+          </button>
         </form>
       </div>
     <FooterComponent/>
