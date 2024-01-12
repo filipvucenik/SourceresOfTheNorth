361e28f4118183793d392cd1894082cee59f43e1
b-mikulan <bruno.mikulan@fer.hr>
2024-01-12 22:37:52 +0100

    delete kod admin i report ispravak

--- IzvorniKod/server/src/main/frontend/src/Report.jsx
+++ IzvorniKod/server/src/main/frontend/src/Report.jsx
@@ -7,13 +7,16 @@ import HeaderComponent from "./HeaderComponent";
 
 const server = apiConfig.getUrl;
 
-const Report = ({ id }) => {
-  console.log(id.id);
+function Reports() {
+  const iddd = useParams();
+
+  console.log(iddd);
+
   const [data, setData] = useState({});
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`${server}reports/${id.id}`);
+      const response = await fetch(`${server}/reports/${iddd.id}`);
       const result = await response.json();
       setData(result);
       console.log(result);
@@ -27,40 +30,37 @@ const Report = ({ id }) => {
   }, []);
 
   return (
-    <>
-      <HeaderComponent />
-      <div className="Report">
-        <div className="report-left">
-          <div>
-            <img src="https://picsum.photos/400/300" />
-          </div>
-          <div>Naslov: {data.reportHeadline}</div>
-          <div>
-            <p>Opis: {data.description}</p>
-          </div>
-          <div>Datum: {data.reportTS}</div>
-        </div>
-        <div className="report-right">
-          <div>
-            <img src="https://picsum.photos/300/300" />
-            {data.location}
-          </div>
-        </div>
-      </div>
-      <FooterComponent />
-    </>
-  );
-};
-
-function Reports() {
-  const iddd = useParams();
-
-  console.log(iddd);
-
-  return (
     <div>
       <div className="page">
-        <Report id={iddd}></Report>
+        {data.report && (
+          <>
+            <HeaderComponent />
+            <div className="Report">
+              <div className="report-left">
+                <div>
+                  <img src="https://picsum.photos/400/300" />
+                </div>
+                <div>Naslov: {data.report.reportHeadline}</div>
+                <div>
+                  <p>Opis: {data.report.description}</p>
+                </div>
+                <div>
+                  Datum:{" "}
+                  {new Date(data.report.reportTS).toLocaleString("de-DE", {
+                    timeZone: "UTC",
+                  })}
+                </div>
+              </div>
+              <div className="report-right">
+                <div>
+                  <img src="https://picsum.photos/300/300" />
+                  {data.location}
+                </div>
+              </div>
+            </div>
+            <FooterComponent />
+          </>
+        )}
       </div>
     </div>
   );
